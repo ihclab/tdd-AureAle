d9a32985b06005bba89673216506860150eb1a1b
AureAle <aperez33@ucol.mx>
2019-03-21 21:15:48 -0600

    Agregado proyecto de pruebas

---
+++ ordenamiento.Pruebas/PruebasOrdenamiento.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ordenamiento.Pruebas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
