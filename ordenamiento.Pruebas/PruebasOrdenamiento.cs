a17e73336bee7c47ee90fca3dcf58183efb51017
AureAle <aperez33@ucol.mx>
2019-03-25 21:04:15 -0600

    no sirven las pruebas

--- ordenamiento.Pruebas/PruebasOrdenamiento.cs
+++ ordenamiento.Pruebas/PruebasOrdenamiento.cs
@@ -2,12 +2,40 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ordenamiento.Pruebas
 {
-    [TestClass]
-    public class UnitTest1
+   
+   
+    public class pruebasOrdenamiento
     {
-        [TestMethod]
-        public void TestMethod1()
+        private int[] salidaEsperada=new int[] {1,2,3,4,5};
+        private int[] salidaEsperada2=new int[] {1,1,1,1,1};
+        private int[] salidaEsperada3=new int[] {1,4,3,4,2};
+        private ordenamiento.Burbuja ordenar = new ordenamiento.Burbuja();
+        
+        public void TestInvertido()
         {
+            //entradas
+            int[]entrada = new int[] {5,4,3,2,1};
+            //proceso
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        public void TestOrdenado()
+        {
+            //entradas
+            int[]entrada = new int[] {1,2,3,4,5};
+            //proceso
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        public void TestUnos()
+        {
+            //entradas
+            int[]entrada = new int[] {1,1,1,1,1};
+            //proceso
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada2, entrada);
         }
     }
 }
--- ordenamiento/Burbuja.cs
+++ ordenamiento/Burbuja.cs
@@ -8,7 +8,14 @@ namespace ordenamiento
     {
 
         public void Ordenar(int [] arr){
-            throw new NotImplementedException("Crear los casos de prueba");
+            //throw new NotImplementedException("Crear los casos de prueba");
+            for(int i =0;i<arr.Length-1;i++){
+                if(arr[i]> arr[i+1]){
+                    int aux = arr[i];
+                    arr[i] =arr[i+1];
+                    arr[i+1]=aux;
+                }
+            }
         }
     }
 }
